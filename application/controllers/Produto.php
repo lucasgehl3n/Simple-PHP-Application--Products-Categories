b328bada438fe46606192bdbd43dc818a9697b99
lucasgehl3n <46547767+lucasgehl3n@users.noreply.github.com>
2019-01-10 12:52:47 -0200

    Controller Produto
    Adição de funções para exclusão, edição e busca por nome (para barra do menu superior).

--- application/controllers/Produto.php
+++ application/controllers/Produto.php
@@ -36,5 +36,58 @@ class Produto extends CI_Controller {
 
 	}
 
+	public function chamaEditarProduto(){ //Procura as informações do produto específico para montar a tela de edição
+		$idProduto = $this->input->post('idProduto');
+
+		$this->load->model('Produtos_model');
+		$data['produto'] = $this->Produtos_model->buscaProdutoPorId($idProduto);
+
+
+		$this->load->model('Categorias_model');
+		$data['categorias'] = $this->Categorias_model-> listaCategorias();
+
+		$this->load->view('header');
+		$this->load->view('produtos/editaProdutos',$data);
+	}
+
+	public function editarProduto(){
+		$produto['nomeProduto'] = $this->input->post('nomeProduto');
+		$produto['descProduto'] = $this->input->post('descProduto');
+		$produto['valor'] = $this->input->post('valor');
+		$produto['quantidade'] = $this->input->post('quantidade');
+		$produto['idProduto'] = $this->input->post('idProduto');
+
+		$this->load->model('Produtos_model');
+		$this->Produtos_model->editarProduto($produto);
+		$this->listaOpcoes();
+
+	}
+
+
+	public function excluirProduto(){
+		$idProduto = $this->input->post('idProduto');
+
+		$this->load->model('Produtos_model');
+		$statusExclusao = $this->Produtos_model->excluirProduto($idProduto);
+
+		if($statusExclusao==1){
+			$this->buscaProdutoPorCategoria();
+		}
+
+		else{
+			echo "Erro!";
+			$this->listaOpcoes();
+		}
+
+	}
+
+	public function buscaProdutoPorNome(){
+		$pesquisa = $this->input->post('campoPesquisa');
+		$this->load->model('Produtos_model');
+		$data['produtos'] = $this->Produtos_model->buscaProdutoPorNome($pesquisa);
+		$this->load->view('header');
+		$this->load->view('produtos/buscaProdutos',$data);	
+	}
+
 
 }
