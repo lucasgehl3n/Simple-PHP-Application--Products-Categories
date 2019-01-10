c68568542c53abfccf5ad75d055258cb254537d2
lucasgehl3n <46547767+lucasgehl3n@users.noreply.github.com>
2019-01-10 13:03:02 -0200

    Atualizada model da CRUD para Categoria

--- application/models/Categorias_model.php
+++ application/models/Categorias_model.php
@@ -6,7 +6,67 @@ class Categorias_model extends CI_Model {
 		return $this->db->get('categoria')->result_array();
 	}
 
-}	
+	public function inserirCategoria($categoria){
+
+		$data = array(
+			'nome' => $categoria['nome'],
+			'descricao' => $categoria['desc']
+			//'caminhoImagem'
+		);
+
+
+		if ($this->db->insert('categoria', $data)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public function buscaCategoriaPorId($idCategoria){
+
+		$query = $this->db->where('idCategoria',$idCategoria)->get('categoria')->result_array();
+		return $query[0]; 
+	}
+
+	public function editaCategoria($categoria){
+
+		$data = array(
+			'nome' => $categoria['nome'],
+			'descricao' => $categoria['desc']
+		);
+		
+		$this->db->where('idCategoria',$categoria['idCategoria'])->update('categoria', $data);
+	
+		if($this->db->affected_rows()>0){ 
+			return 1; 
+		}
+		
+		return 0;			
+	}
+
+	public function excluirCategoria($idCategoria){
+
+	$this->db->where('categoria_idCategoria',$idCategoria);
+	$this->db->delete('produto'); //deleta associações de produtos */
+
+
+
+
+		$this->db->where('idCategoria',$idCategoria);
+		$query= $this->db->delete('categoria'); 
+		echo $this->db->last_query(); 
+		if($this->db->affected_rows()>0){ 
+					//print_r(mysqli_query($query)) ;
+
+			return 1; 
+		}
+
+		//print_r(mysqli_query($query)) ;
+
+		return 0;
+	}
+
+}
 
 ?>
 
