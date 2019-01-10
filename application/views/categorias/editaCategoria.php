3e747643bc2610170d805d935277dcf75b8bf4f8
lucasgehl3n <46547767+lucasgehl3n@users.noreply.github.com>
2019-01-10 13:11:07 -0200

    Corrigidos títulos das páginas

--- application/views/categorias/editaCategoria.php
+++ application/views/categorias/editaCategoria.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Novo Produto</title>
+	<title>Editar Categoria</title>
 </head>
 
 <body>
--- application/views/categorias/novaCategoria.php
+++ application/views/categorias/novaCategoria.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Novo Produto</title>
+	<title>Nova Categoria</title>
 </head>
 
 <body>
